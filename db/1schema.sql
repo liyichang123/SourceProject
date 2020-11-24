0a1be6266006ef8c8ef3631ed91501d44f9f68bc
liyichang <sl123654>
2020-11-24 17:03:58 +0800

    comiit  all

---
+++ db/1schema.sql
@@ -0,0 +1,20 @@
+-- traceSource 核心表
+create database `traceSource` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource 工作流相关库
+create database `traceSource_ac` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource 任务相关库
+create database `traceSource_job` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource 公众号管理相关库
+create database `traceSource_mp` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource nacos配置相关库
+create database `traceSource_config` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource pay配置相关库
+create database `traceSource_pay` default character set utf8mb4 collate utf8mb4_general_ci;
+
+-- traceSource codegen相关库
+create database `traceSource_codegen` default character set utf8mb4 collate utf8mb4_general_ci;
